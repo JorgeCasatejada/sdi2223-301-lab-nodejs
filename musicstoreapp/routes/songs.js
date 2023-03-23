2c5edc98ef5477c1c38543d3609b09e78e766273
Jorge Casatejada Santamarta <UO282294@uniovi.es>
2023-03-23 16:42:56 +0100

    SDI-2223-301-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -2,9 +2,9 @@ module.exports = function(app) {
     app.get("/songs", function(req, res) {
         let response = "";
         if (req.query.title != null && typeof (req.query.title) != "undefined")
-            response = 'Titulo: ' + req.query.title + '<br>';
+            response = "Título: " + req.query.title + "<br>";
         if (req.query.author != null && typeof (req.query.author) != "undefined")
-            response = 'Autor: ' + req.query.author;
+            response = "Autor: " + req.query.author;
         res.send(response);
     });
     app.get('/add', function(req, res) {
@@ -17,7 +17,20 @@ module.exports = function(app) {
     });
     app.get('/songs/:kind/:id', function(req, res) {
         let response = 'id: ' + req.params.id + '<br>'
-            + 'Tipo de música: ' + req.params.kind;
+            + "Tipo de música: " + req.params.kind;
         res.send(response);
+    });
+    app.post('/songs/add', function (req, res){
+       let response = "Canción agregada: " + req.body.title + "<br>"
+        + " género: " + req.body.kind + "<br>"
+        + " precio: " + req.body.price
+
+        res.send(response);
+    });
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta al patrón promo*');
+    });
+    app.get('/pro*ar', function (req, res) {
+        res.send('Respuesta al patrón pro*ar');
     });
 };
