725ee9590e91249c63b5919ad6f481e4cf201ee2
Jorge Casatejada Santamarta <UO282294@uniovi.es>
2023-03-25 17:39:25 +0100

    SDI-2223-301-8.2-Arquitectura para el acceso a datos.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -16,7 +16,9 @@ require("./routes/authors")(app);
 const { MongoClient } = require("mongodb");
 const url = "mongodb+srv://admin:80ZFwxLAXSBpNexZ@sdi-cluster.cw2mnfi.mongodb.net/?retryWrites=true&w=majority";
 app.set('connectionStrings', url);
-require("./routes/songs.js")(app, MongoClient);
+let songsRepository = require("./repositories/songsRepository.js");
+songsRepository.init(app, MongoClient);
+require("./routes/songs.js")(app, songsRepository);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
