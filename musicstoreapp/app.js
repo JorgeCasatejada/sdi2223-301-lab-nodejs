07646c9dd62f9ffe810eaef4aa297e98454ff5fb
Jorge Casatejada Santamarta <UO282294@uniovi.es>
2023-03-25 17:33:04 +0100

    SDI-2223-301-8.1-Acceso a datos de Mongo desde Node.js.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -12,9 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-require("./routes/songs")(app);
 require("./routes/authors")(app);
-
+const { MongoClient } = require("mongodb");
+const url = "mongodb+srv://admin:80ZFwxLAXSBpNexZ@sdi-cluster.cw2mnfi.mongodb.net/?retryWrites=true&w=majority";
+app.set('connectionStrings', url);
+require("./routes/songs.js")(app, MongoClient);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
